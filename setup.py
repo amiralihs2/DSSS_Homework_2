3c268ab884b54f9848e4c6977f7f6ca08635f024
unknown <amir.ali201920@gmail.com>
2023-11-12 18:30:40 +0100

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz_DSSSHW2',
+    version='1.0.0',
+    description='A math quiz game',
+    author='Amirali Hedayati Sepehri',
+    author_email='amir.ali201920@gmail.com',
+    packages=['math_quiz'],
+    install_requires=[
+        'unittest',
+    ],
+)
